ace9710875737f355b22c19a132f23b8d3fc301e
krezon123 <krezon@yandex.ru>
2024-08-14 11:12:55 +0300

    Homewor6

---
+++ ma1in.js
@@ -0,0 +1,54 @@
+
+// Домашняя работа на 6 урок
+
+// let a = prompt('Введите число от 1 до 3')
+
+// switch(+a){
+//     case 1:
+//         alert('Da')
+//     break
+//     case 2:
+//         alert('Net')
+//     break
+//     default:
+//     alert('Не сработало')
+// }
+
+
+
+// let Sea = prompt('Введите число')
+
+// switch(+Sea){
+//     case 1:
+//     case 2:
+//     case 3:
+//         alert('зима')
+//     break
+//     case 4:
+//     case 5:
+//     case 6:
+//         alert('весна')
+//     break
+//     case 7:
+//     case 8:
+//     case 9:
+//         alert('лето')
+//     break
+//     case 10:
+//     case 11:
+//     case 12:
+//         alert('осень')
+//     break
+
+//   default:
+//     alert('Неверно')
+// }
+
+
+
+
+let user0 = null
+let user1 = "Иван";
+let user2 = "Виктор";
+
+alert(user0 ?? user1 ?? user2 ?? "Аноним");
